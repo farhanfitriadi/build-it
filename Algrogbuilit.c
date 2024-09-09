c0d624df2f4f49b4523c31425ad455d011ec4854
farhanfitriadi <farhanfitriadi600@gmail.com>
2024-09-09 20:01:44 +0800

    pertama

---
+++ Algrogbuilit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int x;
+    scanf("%d", &x);
+    
+    if(x % 2==0)
+    {
+        printf("Genap");
+    }
+    else
+    {
+        printf("Ganjil");
+    }
+        
+    
+    return 0;
+}
